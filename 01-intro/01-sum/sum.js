5108d77d6be13695e3b79d25d65f757ede638e06
Alexey Makarov <alexeimakarow@gmail.com>
2022-04-22 10:59:27 +0200

    add solution for 1-module 1-task

--- 01-intro/01-sum/sum.js
+++ 01-intro/01-sum/sum.js
@@ -1,5 +1,17 @@
 function sum(a, b) {
-  /* ваш код */
+  if (typeof a === 'number' && typeof b === 'number') {
+    return a + b;
+  } else {
+    throw new TypeError('One of arguments is not a number');
+  }
 }
+
+sum(1, 2);
+
+// В модуле sum.js необходимо реализовать функцию, которая принимает два аргумента
+// и возвращает их сумму. Если аргументы не являются числами —
+// функция должна бросать ошибку TypeError.
+// sum(1, 2); // 3
+// sum('1', []); // TypeError
 
 module.exports = sum;

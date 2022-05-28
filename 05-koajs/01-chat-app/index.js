2740b457c07a50b3d15a0d69a45945ad5340c7e5
Alexey Makarov <alexeimakarow@gmail.com>
2022-05-28 18:41:35 +0200

    add solution for 5-module 1-task

--- 05-koajs/01-chat-app/index.js
+++ 05-koajs/01-chat-app/index.js
@@ -1,5 +1,5 @@
 const app = require('./app');
 
-app.listen(3000, () => {
-  console.log('App is running on http://localhost:3000');
+app.listen(3333, () => {
+  console.log('App is running on http://localhost:3333');
 });

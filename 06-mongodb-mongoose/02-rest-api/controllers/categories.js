82b06b880d30592115d74ec700d8c76df59b0040
Alexey Makarov <alexeimakarow@gmail.com>
2022-05-29 16:56:25 +0200

    add solution for 6-module 1-task

--- 06-mongodb-mongoose/02-rest-api/controllers/categories.js
+++ 06-mongodb-mongoose/02-rest-api/controllers/categories.js
@@ -1,3 +1,23 @@
+const Category = require('../models/Category');
+
 module.exports.categoryList = async function categoryList(ctx, next) {
-  ctx.body = {categories: []};
+  const categoriesData = await Category.find();
+  let categories = [];
+
+  if (categoriesData && categoriesData.length) {
+    categories = categoriesData.map((category) => {
+      return {
+        id: category._id.toString(),
+        title: category.title,
+        subcategories: category.subcategories.map((item) => {
+          return {
+            id: item._id.toString(),
+            title: item.title,
+          };
+        }),
+      };
+    });
+  }
+
+  ctx.body = {categories};
 };

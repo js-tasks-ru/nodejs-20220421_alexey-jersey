1097897c2b6fc653025a451d5e32a52a73c9062a
Alexey Makarov <alexeimakarow@gmail.com>
2022-06-09 20:26:49 +0200

    add solution for 8-module 1-task

--- 08-email/01-registration/controllers/registration.js
+++ 08-email/01-registration/controllers/registration.js
@@ -1,11 +1,49 @@
-const { v4: uuid } = require('uuid');
+const {v4: uuid} = require('uuid');
 const User = require('../models/User');
 const sendMail = require('../libs/sendMail');
 
 module.exports.register = async (ctx, next) => {
+  const token = uuid();
 
+  const {displayName, email, password} = ctx.request.body;
+
+  const isUserExist = await User.findOne({email});
+
+  if (isUserExist) {
+    ctx.status = 400;
+    ctx.body = {errors: {
+      email: 'Такой email уже существует',
+    }};
+  }
+
+  const user = new User({displayName, email, verificationToken: token});
+  await user.setPassword(password);
+  await User.create(user);
+
+  await sendMail({
+    template: 'confirmation',
+    locals: {token},
+    to: 'newuser@mail.com',
+    subject: 'Подтвердите почту',
+  });
+
+  ctx.status = 200;
+  ctx.body = {status: 'ok'};
+  next(ctx);
 };
 
 module.exports.confirm = async (ctx, next) => {
+  const {verificationToken} = ctx.request.body;
 
+  const user = await User.findOneAndUpdate(
+      {verificationToken},
+      {$unset: {verificationToken: ''}},
+  );
+
+  if (!user) {
+    ctx.status = 400;
+    ctx.body = {error: 'Ссылка подтверждения недействительна или устарела'};
+  } else {
+    ctx.body = {token: verificationToken};
+  }
 };

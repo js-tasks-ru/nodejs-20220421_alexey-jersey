2ac8f89e0a39e63de772e303f3391de8b4bbf312
Alexey Makarov <alexeimakarow@gmail.com>
2022-06-11 16:11:47 +0200

    add solution for 8-module 2-task

--- 08-email/02-order/controllers/orders.js
+++ 08-email/02-order/controllers/orders.js
@@ -1,8 +1,51 @@
+const Product = require('../models/Product');
 const Order = require('../models/Order');
 const sendMail = require('../libs/sendMail');
 
 module.exports.checkout = async function checkout(ctx, next) {
+  if (!ctx.user) {
+    ctx.status = 401;
+    return next();
+  }
+
+  const {product, phone, address} = ctx.request.body;
+
+  const order = await Order.create({
+    user: ctx.user.id,
+    product,
+    phone,
+    address,
+  });
+
+  if (!order) {
+    ctx.status = 400;
+    return next();
+  }
+
+  const productData = await Product.findOne({id: order.product});
+
+  ctx.body = {
+    order: order._id,
+    product: product._id,
+    phone: order.phone,
+    address: order.address,
+  };
+
+  await sendMail({
+    template: 'order-confirmation',
+    locals: {id: order._id.toString(), product: productData},
+    to: ctx.user.email,
+    subject: 'Подтвердите заказ',
+  });
 };
 
 module.exports.getOrdersList = async function ordersList(ctx, next) {
+  if (!ctx.user) {
+    ctx.status = 401;
+    return next();
+  }
+
+  const orders = await Order.find({user: ctx.user.id}).populate('product');
+
+  ctx.body = {orders};
 };

5b9b1ded93d03e74211ba2f0e789ce85f4e50ece
Alexey Makarov <alexeimakarow@gmail.com>
2022-06-13 19:56:25 +0200

    add solution for 9-module 1-task

--- 09-websockets/01-chat/controllers/messages.js
+++ 09-websockets/01-chat/controllers/messages.js
@@ -1,5 +1,24 @@
 const Message = require('../models/Message');
 
 module.exports.messageList = async function messages(ctx, next) {
-  ctx.body = {};
+  if (!ctx.user) {
+    ctx.status = 401;
+    ctx.body = {error: 'Пользователь не залогинен'};
+    return next();
+  }
+
+  const data = await Message.find({
+    chat: ctx.user._id,
+  }).limit(20);
+
+  const messages = data.map((message) => {
+    return {
+      date: message.date,
+      text: message.text,
+      id: message._id,
+      user: message.user,
+    };
+  });
+
+  ctx.body = {messages};
 };
--- 09-websockets/01-chat/socket.js
+++ 09-websockets/01-chat/socket.js
@@ -4,14 +4,40 @@ const Session = require('./models/Session');
 const Message = require('./models/Message');
 
 function socket(server) {
-  const io = socketIO(server);
+  const io = socketIO(server, {
+    cors: {
+      origin: 'http://localhost',
+      methods: ['GET', 'POST'],
+      credentials: true,
+      transports: ['websocket', 'polling'],
+    },
+    allowEIO3: true,
+  });
 
   io.use(async function(socket, next) {
+    const token = socket.handshake.query.token;
+
+    if (!token) {
+      next(new Error('anonymous sessions are not allowed'));
+    }
+    const session = await Session.findOne({token}).populate('user');
+
+    if (session) {
+      socket.user = session.user;
+    }
+
     next();
   });
 
   io.on('connection', function(socket) {
-    socket.on('message', async (msg) => {});
+    socket.on('message', async (msg) => {
+      await Message.create({
+        date: new Date(),
+        text: msg,
+        chat: socket.user._id,
+        user: socket.user.displayName,
+      });
+    });
   });
 
   return io;
